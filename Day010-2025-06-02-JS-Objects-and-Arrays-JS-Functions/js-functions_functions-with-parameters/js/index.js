62a18e3821ba46d8e4a3edad743159b7a41da40c
Felipe <lipejdc@gmail.com>
2025-06-02 15:40:06 +0200

    finish challenge

--- Day010-2025-06-02-JS-Objects-and-Arrays-JS-Functions/js-functions_functions-with-parameters/js/index.js
+++ Day010-2025-06-02-JS-Objects-and-Arrays-JS-Functions/js-functions_functions-with-parameters/js/index.js
@@ -1,20 +1,18 @@
 console.clear();
 
-console.log("-------------");
-console.log("Exercise 1:");
+logSeparator(1);
 /*
 1: The function below is not executed. Can you figure out why? Find the bug and fix it.
 */
 
 // Uncomment the next line of code and figure out why it does not work:
-// logText;
+logText();
 
 function logText() {
   console.log("This text does not appear. Why?");
 }
 
-console.log("-------------");
-console.log("Exercise 2:");
+logSeparator(2);
 /*
 2: Look at the functions below. They are almost identical. Can you find a way to generalize them into a new function with an input parameter? 
    Replace the function calls below with your new function.
@@ -38,12 +36,17 @@ greetMary();
 
 // --v-- write your code here --v--
 
-function greet() {}
+function greetPerson(personName) {
+  console.log(`Welcome ${personName}, good to see you again!`);
+}
+
+greetPerson("Alice");
+greetPerson("Bob");
+greetPerson("Mary");
 
 // --^-- write your code here --^--
 
-console.log("-------------");
-console.log("Exercise 3:");
+logSeparator(3);
 /*
 3: We log a small separation text for each subtask. This is repetitive code, so let’s write a function `logSeparator` that takes the number of the exercise as an input parameter and logs the corresponding separator.
    Then, use this function to replace the existing separator logs in this JavaScript file.
@@ -52,5 +55,18 @@ Note: Separation text refers to the text that is logged in the console before ea
 */
 
 // --v-- write your code here --v--
-
+function logSeparator(exerciseNumber) {
+  console.log("-------------");
+  console.log(`Exercise ${exerciseNumber}:`);
+}
 // --^-- write your code here --^--
+
+
+logText();
+greetPerson("Alice");
+greetPerson("Bob");
+greetPerson("Mary");
+logSeparator(1);
+logSeparator(2);
+logSeparator(3);
+
