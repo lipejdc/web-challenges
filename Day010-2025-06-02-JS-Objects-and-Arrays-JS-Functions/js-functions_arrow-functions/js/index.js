0676290168c26576f24586f0b7a76013e1d34b9a
Felipe <lipejdc@gmail.com>
2025-06-02 18:26:36 +0200

    finish challenge

--- Day010-2025-06-02-JS-Objects-and-Arrays-JS-Functions/js-functions_arrow-functions/js/index.js
+++ Day010-2025-06-02-JS-Objects-and-Arrays-JS-Functions/js-functions_arrow-functions/js/index.js
@@ -21,13 +21,32 @@ function getCurrentHour() {
   }
 }
 
+const getCurrentHourAsArrowFunction = () => {
+  const now = new Date();
+  const currentHour = now.getHours();
+  if (currentHour === 0) {
+    return "12am";
+  } else if (currentHour === 12) {
+    return "12pm";
+  } else if (currentHour <= 11) {
+    return currentHour + "am";
+  } else {
+    return currentHour - 12 + "pm";
+  }
+};
+
 function getVectorLength(x, y, z) {
   return (x ** 2 + y ** 2 + z ** 2) ** 0.5;
 }
 
+const getVectorLengthAsArrowFunction = (x, y, z) =>
+  (x ** 2 + y ** 2 + z ** 2) ** 0.5;
+
 function cleanInput(string) {
   return string.toLowerCase().trim();
 }
+
+const cleanInputAsArrowFunction = (string) => string.toLowerCase().trim();
 
 /*
 Rewrite the following arrow functions as classic functions.
@@ -45,6 +64,26 @@ const isOddAndSmall = (number) => {
   return true;
 };
 
+function isOddAndSmallAsNormalFunction(number) {
+  if (number > 10) {
+    return false;
+  }
+
+  if (number % 2 === 0) {
+    return false;
+  }
+
+  return true;
+}
+
 const add3 = (a, b, c) => a + b + c;
 
+function add3AsNormalFunction(a, b, c) {
+  return a + b + c;
+}
+
 const repeat10 = (string) => string.repeat(10);
+
+function repeat10AsNormalFunction(string) {
+  return string.repeat(10);
+}
