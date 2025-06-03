f6bc9b07b54b48e23148186fd41d16b84e1fb93d
Felipe <lipejdc@gmail.com>
2025-06-03 14:32:05 +0200

    finish challenge

--- Day011-2026-06-03-HTML-Forms-JS-Inputs-and-Strings/js-inputs-and-strings_input-to-uppercase/js/index.js
+++ Day011-2026-06-03-HTML-Forms-JS-Inputs-and-Strings/js-inputs-and-strings_input-to-uppercase/js/index.js
@@ -1,1 +1,7 @@
-console.clear();
+const firstInput = document.getElementById('first-input');
+const uppercaseButton = document.querySelector('[data-js="button-uppercase"]');
+
+
+uppercaseButton.addEventListener("click", () => {
+    firstInput.value = firstInput.value.toUpperCase();
+});
