506cc3e79938e275d07723b06aab216e604f7dac
Felipe <lipejdc@gmail.com>
2025-06-04 11:17:54 +0200

    finish challenge

--- Day012-2026-06-04-JS-Forms-JS-createElement/js-forms_handle-form-submit/js/index.js
+++ Day012-2026-06-04-JS-Forms-JS-createElement/js-forms_handle-form-submit/js/index.js
@@ -1,3 +1,29 @@
 console.clear();
 
 const form = document.querySelector('[data-js="form"]');
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const formData = new FormData(event.target);
+  const data = Object.fromEntries(formData);
+
+  console.log(data);
+
+  const firstNameInputValue = event.target.elements.firstName.value;
+  console.log(firstNameInputValue);
+
+  const ageInputValueToNumber = Number(event.target.elements.age.value);
+  console.log(ageInputValueToNumber);
+
+  const badnessInputValueToNumber = Number(event.target.elements.badness.value);
+  console.log(badnessInputValueToNumber);
+
+  const ageBadnessSum = ageInputValueToNumber + badnessInputValueToNumber;
+  console.log(ageBadnessSum);
+
+  console.log(`The age-badness-sum of ${firstNameInputValue} is ${ageBadnessSum}!`);
+
+
+  event.target.reset();
+});
