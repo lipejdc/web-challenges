ca08c8ce9bd43541b50c86a64a586b0fecaee243
Felipe <lipejdc@gmail.com>
2025-06-04 12:25:31 +0200

    finish challenge

--- Day012-2026-06-04-JS-Forms-JS-createElement/js-forms_checkbox-input/js/index.js
+++ Day012-2026-06-04-JS-Forms-JS-createElement/js-forms_checkbox-input/js/index.js
@@ -3,6 +3,7 @@ console.clear();
 const form = document.querySelector('[data-js="form"]');
 const tosError = document.querySelector('[data-js="tos-error"]');
 const tosCheckbox = document.querySelector('[data-js="tos"]');
+const successMessage = document.querySelector('[data-js="success"]');
 
 function hideTosError() {
   tosError.setAttribute("hidden", "");
@@ -12,13 +13,44 @@ function showTosError() {
   tosError.removeAttribute("hidden");
 }
 
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
+function hideSuccessMessage() {
+  successMessage.setAttribute("hidden", "");
+}
+
+function showSuccessMessage() {
+  successMessage.removeAttribute("hidden");
+}
+
+hideTosError();
+hideSuccessMessage();
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
 
   // --v-- write your code here --v--
+  const isTosCheckboxChecked = e.target.elements.tos.checked;
+  console.log(isTosCheckboxChecked);
+
+  if (isTosCheckboxChecked) {
+    alert("Form submitted");
+    showSuccessMessage();
+    return;
+  }
+
+  showTosError();
+  
 
   // --^-- write your code here --^--
 
-  // eslint-disable-next-line no-alert
-  alert("Form submitted");
 });
+
+tosCheckbox.addEventListener("change", (e) => {
+  
+  const isTosCheckboxChecked = e.target.checked;
+  console.log(isTosCheckboxChecked);
+
+  if (isTosCheckboxChecked) {
+  hideTosError();
+} else {
+  showTosError();
+}
+})
