0324303dfbc0e948e69970e99866d8ce10b7409a
Felipe <lipejdc@gmail.com>
2025-06-05 00:57:12 +0200

    finish challenge

--- Day012-2026-06-04-JS-Forms-JS-createElement/js-forms_calculator/js/index.js
+++ Day012-2026-06-04-JS-Forms-JS-createElement/js-forms_calculator/js/index.js
@@ -22,10 +22,33 @@ function divide(a, b) {
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
+  const formData = new FormData(event.target);
+  const data = Object.fromEntries(formData);
+  const firstNumber = data.numberA;
+  const secondNumber = data.numberB;
+  const operator = data.operator;
   let result;
 
   // --v-- write your code here --v--
-
+  switch (operator) {
+    case "addition":
+      result = add(Number(firstNumber), Number(secondNumber));
+      console.log(result);
+      break;
+    case "subtraction":
+      result = subtract(Number(firstNumber), Number(secondNumber));
+      console.log(result);
+      break;
+    case "multiplication":
+      result = multiply(Number(firstNumber), Number(secondNumber));
+      console.log(result);
+      break;
+    case "division":
+      result = divide(Number(firstNumber), Number(secondNumber));
+      console.log(result);
+      break;
+    // other cases...
+  }
   // --^-- write your code here --^--
 
   resultOutput.textContent = result;
