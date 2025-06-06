d6a2cd5fd017a696decf3a207048ad228ee2b3c9
Felipe <lipejdc@gmail.com>
2025-06-06 17:14:29 +0200

    finish challenge

--- Day014-2025-06-06-Recap-Project-2-and-JS-Loops/js-loops_stars/js/index.js
+++ Day014-2025-06-06-Recap-Project-2-and-JS-Loops/js-loops_stars/js/index.js
@@ -2,13 +2,31 @@ console.clear();
 
 const starContainer = document.querySelector('[data-js="star-container"]');
 
-function renderStars() {
+function renderStars(filledStars) {
+
+  const filledStarSrc = "assets/star-filled.svg";
+  const emptyStarSrc = "assets/star-empty.svg";
+
   // Reset the star container before re-rendering stars
   starContainer.innerHTML = "";
 
   // --v-- write or modify code below this line --v--
 
+  for (let i = 1; i <= 5; i++) {
+    const newStarImageElement = document.createElement("img");
+
+    newStarImageElement.src = i <= filledStars
+      ? filledStarSrc
+      : emptyStarSrc
+
+    newStarImageElement.addEventListener("click", () => {
+      renderStars(i);
+    })
+
+    starContainer.append(newStarImageElement);
+  }
+
   // --^-- write or modify code above this line --^--
 }
 
-renderStars();
+renderStars(3);
