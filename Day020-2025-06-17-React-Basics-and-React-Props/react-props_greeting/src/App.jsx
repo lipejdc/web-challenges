5169b0bb7c790155c9d3f808cffcf465e45825b8
Felipe <lipejdc@gmail.com>
2025-06-17 15:15:24 +0200

    finish challenge

--- Day020-2025-06-17-React-Basics-and-React-Props/react-props_greeting/src/App.jsx
+++ Day020-2025-06-17-React-Basics-and-React-Props/react-props_greeting/src/App.jsx
@@ -1,3 +1,12 @@
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return (
+    <>
+      <Greeting name="Felipe" />
+      <Greeting name="Roland" />
+    </>
+  );
+}
+
+function Greeting({ name }) {
+  return <span>Hello, {name === "Roland" ? "Coach" : name}!</span>;
 }
