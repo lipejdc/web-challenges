c77da99bc110423f550b356b813ce806b8c1df40
Felipe <lipejdc@gmail.com>
2025-06-17 16:06:07 +0200

    finish challenge

--- Day020-2025-06-17-React-Basics-and-React-Props/react-props_buttons/src/App.jsx
+++ Day020-2025-06-17-React-Basics-and-React-Props/react-props_buttons/src/App.jsx
@@ -1,3 +1,59 @@
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  // 4. Moved the event handler to App component
+  function handleClick(message) {
+    alert(message);
+  }
+
+  return (
+    <>
+      <Button
+        text="Black button"
+        color="black"
+        disabled
+        backgroundColor="white"
+        onClick={() => handleClick("You clicked the black text button!!")}
+      />
+      <Button
+        text="Blue button"
+        color="blue"
+        disabled={false}
+        backgroundColor="yellow"
+        onClick={() => handleClick("You clicked the blue text button!!")}
+      />
+      <Button
+        text="Red button"
+        color="red"
+        disabled
+        backgroundColor="grey"
+        onClick={() => handleClick("You clicked the red text button!!")}
+      />
+      <Button
+        text="Green button"
+        color="green"
+        disabled={false}
+        backgroundColor="black"
+        onClick={() => handleClick("You clicked the green text button!!")}
+      />
+    </>
+  );
+}
+
+function Button({ text, color, disabled, backgroundColor, onClick }) {
+  return (
+    <button
+      onClick={onClick}
+      disabled={disabled}
+      style={{
+        color: color,
+        backgroundColor: backgroundColor,
+        height: "6rem",
+        fontSize: "1.25rem",
+        border: "1px solid black",
+        padding: "0.75rem 1.5rem",
+        cursor: disabled ? "not-allowed" : "pointer",
+      }}
+    >
+      {text}
+    </button>
+  );
 }
