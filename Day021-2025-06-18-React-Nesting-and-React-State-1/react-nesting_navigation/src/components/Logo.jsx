c9b3fd6028c6f63e4ab0ccd84d5722be458a33eb
Felipe <lipejdc@gmail.com>
2025-06-18 11:25:45 +0200

    finish challenge

---
+++ Day021-2025-06-18-React-Nesting-and-React-State-1/react-nesting_navigation/src/components/Logo.jsx
@@ -0,0 +1,10 @@
+import logo from "../img/logo.jpg";
+import Image from "./Image";
+
+export default function Logo() {
+    return (
+        <>
+        <Image src={logo} alt="logo"/>
+        </>
+    )
+}
