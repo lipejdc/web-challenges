ee4ee7b539f0f5671f502459711d78500db30772
Felipe <lipejdc@gmail.com>
2025-06-19 11:19:03 +0200

    finish challenge

--- Day021-2025-06-18-React-Nesting-and-React-State-1/react-state-1_journal-app-favorite-button/src/components/FavoriteButton/index.jsx
+++ Day021-2025-06-18-React-Nesting-and-React-State-1/react-state-1_journal-app-favorite-button/src/components/FavoriteButton/index.jsx
@@ -1,17 +1,20 @@
+import { useState } from "react";
 import "./FavoriteButton.css";
 import StarFilled from "./star-filled.svg?react";
 import Star from "./star.svg?react";
 
 export default function FavoriteButton() {
-  // This should be a state variable.
-  const isFavorite = false;
+
+  const [isFavorite, setIsFavorite] = useState();
+
+  function handleFavoriteClick() {
+    setIsFavorite((prevFavorite) => !prevFavorite);
+  }
 
   return (
     <button
       className="favorite-button"
-      onClick={() => {
-        console.log("favorite button clicked");
-      }}
+      onClick={handleFavoriteClick}
       aria-label="favorite"
     >
       {isFavorite ? <StarFilled /> : <Star />}
