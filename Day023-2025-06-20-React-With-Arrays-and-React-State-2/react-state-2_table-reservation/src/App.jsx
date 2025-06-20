6f458fded86df76a13d468c72ccc7ef5f0112441
Felipe <lipejdc@gmail.com>
2025-06-20 18:00:06 +0200

    finish challenge

--- Day023-2025-06-20-React-With-Arrays-and-React-State-2/react-state-2_table-reservation/src/App.jsx
+++ Day023-2025-06-20-React-With-Arrays-and-React-State-2/react-state-2_table-reservation/src/App.jsx
@@ -1,12 +1,27 @@
 import Counter from "./components/Counter";
+import { useState } from "react";
 import "./styles.css";
 
 export default function App() {
+  const [people, setPeople] = useState(0);
+
+  const handleIncrementClick = () => {
+    setPeople((prevPeople) => prevPeople + 1);
+  };
+
+  const handleDecrementClick = () => {
+    setPeople((prevPeople) => prevPeople - 1);
+  };
+
   return (
     <div className="container">
       <h1>Place a Table Reservation</h1>
-      <Counter />
-      <p>You are going to reserve a table for 2 people.</p>
+      <Counter
+        numPeople={people}
+        onIncrementPeople={handleIncrementClick}
+        onDecrementPeople={handleDecrementClick}
+      />
+      <p>{people}</p>
     </div>
   );
 }
