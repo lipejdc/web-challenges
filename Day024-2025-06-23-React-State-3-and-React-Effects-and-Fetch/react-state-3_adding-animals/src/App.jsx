076ae471c52c3fcdda92a728bd141d54d4d0cfc3
Felipe <lipejdc@gmail.com>
2025-06-23 11:42:21 +0200

    finish challenge

--- Day024-2025-06-23-React-State-3-and-React-Effects-and-Fetch/react-state-3_adding-animals/src/App.jsx
+++ Day024-2025-06-23-React-State-3-and-React-Effects-and-Fetch/react-state-3_adding-animals/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "./styles.css";
 import Form from "./components/Form";
 import List from "./components/List";
+import { uid } from "uid";
 
 const initialAnimals = [
   {
@@ -25,7 +26,11 @@ export default function App() {
   const [animals, setAnimals] = useState(initialAnimals);
 
   function handleAddAnimal(newAnimal) {
-    console.log(newAnimal);
+    
+    setAnimals((prevAnimals => {
+     const updatedAnimals = [...prevAnimals, {id: uid(), ...newAnimal}];
+     return updatedAnimals;
+    }))
   }
 
   return (
