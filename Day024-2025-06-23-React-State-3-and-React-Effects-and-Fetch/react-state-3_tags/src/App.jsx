59f6cad35893fad6c8681b501dbbcb0c73b65796
Felipe <lipejdc@gmail.com>
2025-06-23 12:03:19 +0200

    finish challenge

--- Day024-2025-06-23-React-State-3-and-React-Effects-and-Fetch/react-state-3_tags/src/App.jsx
+++ Day024-2025-06-23-React-State-3-and-React-Effects-and-Fetch/react-state-3_tags/src/App.jsx
@@ -6,10 +6,15 @@ import List from "./components/List";
 export default function App() {
   const [tags, setTags] = useState(["JavaScript", "React", "CSS", "HTML"]);
 
+  const handleAddTag = (newTag) => setTags((prevTags) => [...prevTags, newTag]);
+
+  const handleDeleteTag = (tagToDelete) =>
+    setTags(tags.filter((tag) => tag !== tagToDelete));
+
   return (
     <main className="app">
-      <Form />
-      <List tags={tags} />
+      <Form onAddTag={handleAddTag} />
+      <List tags={tags} onDeleteTag={handleDeleteTag} />
     </main>
   );
 }
