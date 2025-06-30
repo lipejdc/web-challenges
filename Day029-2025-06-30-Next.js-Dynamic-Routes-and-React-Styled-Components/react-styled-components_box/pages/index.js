e6b68efa9224d76631681f16e258323185a5572b
Felipe <lipejdc@gmail.com>
2025-06-30 16:37:21 +0200

    finish challenge

--- Day029-2025-06-30-Next.js-Dynamic-Routes-and-React-Styled-Components/react-styled-components_box/pages/index.js
+++ Day029-2025-06-30-Next.js-Dynamic-Routes-and-React-Styled-Components/react-styled-components_box/pages/index.js
@@ -1,10 +1,14 @@
 import BoxWithClassName from "../components/BoxWithClassName/BoxWithClassName.js";
+import BoxWithStylesComponents from "@/components/BoxWithStyledComponents/BoxWithStylesComponents.js";
 
 export default function HomePage() {
   return (
     <div>
       <BoxWithClassName />
       <BoxWithClassName isBlack />
+
+      <BoxWithStylesComponents />
+      <BoxWithStylesComponents $isBlack />
     </div>
   );
 }
