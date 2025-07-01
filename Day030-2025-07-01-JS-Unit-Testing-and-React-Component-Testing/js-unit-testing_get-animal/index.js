3c67ba74771241d4e2d4574b0ef550fa114e845d
Felipe <lipejdc@gmail.com>
2025-07-01 10:54:59 +0200

    finish challenge

--- Day030-2025-07-01-JS-Unit-Testing-and-React-Component-Testing/js-unit-testing_get-animal/index.js
+++ Day030-2025-07-01-JS-Unit-Testing-and-React-Component-Testing/js-unit-testing_get-animal/index.js
@@ -1,1 +1,17 @@
-export function getAnimal() {}
+// export function getAnimal(animal) {
+//     if (animal === "cats") {
+//         return `I totally love ${animal}!`;
+//     }
+//     else if (!animal) {
+//         return "I do not like animals at all!";
+//     }
+//     else {
+//         return `I like ${animal}!`;
+//     }
+// }
+
+export function getAnimal(animal) {
+  if (!animal) return "I do not like animals at all!";
+  if (animal === "cats") return "I totally love cats!";
+  return `I like ${animal}!`;
+}
