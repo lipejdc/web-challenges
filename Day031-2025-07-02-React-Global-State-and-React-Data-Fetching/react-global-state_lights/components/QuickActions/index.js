32e213a082dfc0c97a17af27892f59ffea67ddae
Felipe <lipejdc@gmail.com>
2025-07-02 15:34:08 +0200

    finish challenge

--- Day031-2025-07-02-React-Global-State-and-React-Data-Fetching/react-global-state_lights/components/QuickActions/index.js
+++ Day031-2025-07-02-React-Global-State-and-React-Data-Fetching/react-global-state_lights/components/QuickActions/index.js
@@ -7,22 +7,18 @@ const StyledQuickActions = styled.div`
   gap: 16px;
 `;
 
-export default function QuickActions() {
+export default function QuickActions( { onLightsOff, onLightsOn }) {
   return (
     <StyledQuickActions>
       <Button
         type="button"
-        onClick={() => {
-          console.log("Turn all lights off");
-        }}
+        onClick={onLightsOff}
       >
         Turn all lights off
       </Button>
       <Button
         type="button"
-        onClick={() => {
-          console.log("Turn all lights on");
-        }}
+        onClick={onLightsOn}
       >
         Turn all lights on
       </Button>
